bba0a2400c3ce6bfbff9764beff4adba668a1c75
YuanSandra <Yuansandra123@gmail.com>
2017-10-24 19:50:09 -0700

    output bermacam gaya

--- praktek0202.cpp
+++ praktek0202.cpp
@@ -10,4 +10,7 @@ int main(){
 	double angka(10.5),Angka,ANGKA(3.14);
 	int a(10),b(7),c(12);
 	
+	cout << nilai<<" : "<<Nilai<<" | "<<NILAI<<" "<<niLAi<<endl;
+	cout << angka <<"/n";
+	cout << Angka <<endl<<ANGKA;
 }
