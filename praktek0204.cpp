9486086a4ce499e32d7dbecb4a2bba953b9b8d03
YuanSandra <Yuansandra123@gmail.com>
2017-10-24 20:15:59 -0700

    luas rumah 42m2

---
+++ praktek0204.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std; 
+
+int main (){
+	double luasrumah = 42; 
+	double lantaipkt = 193*1215;
+	double luas1box= lantaipkt*8/1000000;
+	double jumlahboxP,biayaP,biayaPG,totalB;
+	
+	cout <<"luas rumah 	  :"<<luasrumah<<"m2"<<endl;
+	
+}
