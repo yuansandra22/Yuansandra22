180c6dcdf7edb5c3cb8e9a5b7efec50cc6241228
YuanSandra <Yuansandra123@gmail.com>
2018-01-26 11:50:17 +0700

    PEMBAGIAN CLASS PRODUK SUSU YA

---
+++ PEMBAGIAN CLASS PRODUK SUSU YA.cpp
@@ -0,0 +1,35 @@
+#include <stdio.h>
+#include <conio.h>
+#include <stdlib.h>
+main (void)
+{
+ 
+printf("\n");
+printf("\n");
+puts("                     www.YuanSandra.com                ");
+puts("                    Jl. MANDIRI II No.17,5                       ");
+puts("                     TELP 0895383556258                 ");
+puts("                     JUALAN PRODUK SUSU                 ");
+printf("\n");
+printf("================================================================\n");
+printf("\n");
+   
+printf("*****[PILIHAN PRODUK]*****\n");
+printf("A. Susu Dancow\n");
+printf("    1. Ukuran Kecil\n");
+printf("    2. Ukuran Sedang\n");
+printf("    3. Ukuran Besar\n");
+printf("B. Susu Bendera\n");
+printf("    1. Ukuran Kecil\n");
+printf("    2. Ukuran Sedang\n");
+printf("    3. Ukuran Besar\n");
+printf("C. Susu SGM\n");
+printf("    1. Ukuran Kecil\n");
+printf("    2. Ukuran Sedang\n");
+printf("    3. Ukuran Besar\n");
+printf("=================================================================\n");
+
+
+
+   
+}
